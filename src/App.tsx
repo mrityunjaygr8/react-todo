2b65fb83a3a23f1e54829c379e9fa390357ed4d8
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-11-05 17:24:11 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { AppDispatch, RootState } from "./redux/store";
+
+import { addTodo, removeTodo, setTodoStatus } from "./redux/todoSlice";
+
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Checkbox from "@material-ui/core/Checkbox";
+
+
+function App() {
+  const [todoDescription, setTodoDescription] = useState("");
+
+  const todoList = useSelector((state: RootState) => state);
+  const dispatch = useDispatch<AppDispatch>();
+
+  return (
+    <Container maxWidth="xs">
+      <Typography style={{ textAlign: "center" }} variant="h3">
+        Redux List App
+      </Typography>
+      <TextField
+        variant="outlined"
+        label="To Do Item"
+        fullWidth
+        onChange={(e) => setTodoDescription(e.target.value)}
+        value={todoDescription}
+      />
+      <Button variant="contained" color="primary" fullWidth 
+        onClick={()=>{
+          dispatch(addTodo(todoDescription));
+          setTodoDescription("");
+        }}>
+        Add Todo
+      </Button>
+
+      <List>
+      {todoList.map((todo) => (
+        <ListItem key={todo.id}>
+          <ListItemText style={{ textDecoration: todo.completed ? "line-through" : "none" }}>{todo.description}</ListItemText>
+          <ListItemSecondaryAction>
+            <IconButton onClick={() => {dispatch(removeTodo(todo.id))}}>
+              <DeleteIcon />
+            </IconButton>
+            <Checkbox edge="end" value={todo.completed}
+              onChange={() => {dispatch(setTodoStatus({id: todo.id, completed: !todo.completed}))}} />
+              
+
+            
+          </ListItemSecondaryAction>
+        </ListItem>
+      
+      ))}
+      </List>
+    </Container>
+  )
+}
+
+export default App
