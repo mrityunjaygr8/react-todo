50c31214be08805ca19461a02230492615a0bda6
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2021-11-05 17:44:22 +0530

    unused imports removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { useSelector, useDispatch } from "react-redux";
-import { AppDispatch, RootState } from "./redux/store";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
 
 
-import { removeTodo, setTodoStatus } from "./redux/todoSlice";
 
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
-import Checkbox from "@material-ui/core/Checkbox";
 
 import MyInput from "./components/input";
 import TodoItem from "./components/todoItem";
@@ -21,7 +14,6 @@ import TodoItem from "./components/todoItem";
 function App() {
 
   const todoList = useSelector((state: RootState) => state);
-  const dispatch = useDispatch<AppDispatch>();
 
   return (
     <Container maxWidth="xs">
